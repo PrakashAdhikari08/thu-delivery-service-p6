e0fd535bfd105a6325f1ecc4d8627a9693b6079c
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2023-09-14 20:08:56 +1000

    CI Fix

--- src/test/java/com/superprice/deliveryservice/DeliveryServiceApplicationTests.java
+++ src/test/java/com/superprice/deliveryservice/DeliveryServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.superprice.deliveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 //@SpringBootTest
+@Profile("test")
 class DeliveryServiceApplicationTests {
 
 //	@Test
